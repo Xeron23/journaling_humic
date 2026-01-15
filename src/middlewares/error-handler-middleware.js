c1ba532c7a92dea56ce3eaa874af8d7850c1e5ed
Aria Lent <kaqyword2304@gmail.com>
2026-01-15 11:50:10 +0700

    fix:error handler

--- src/middlewares/error-handler-middleware.js
+++ src/middlewares/error-handler-middleware.js
@@ -5,90 +5,94 @@ import { INVALID_CREDENTIALS, SERVER_PROBLEM } from "../errors/error-codes.js";
 import useragent from "useragent";
 import prisma from "../config/db.js";
 
+export const errorHandler = async (err, req, res, _next) => {
+  const statusCode = Object.values(StatusCodes).find(
+    (code) => code.message === err.statusCode
+  );
 
-export const errorHandler = async(err, req, res, _next) => {
-    const statusCode = Object.values(StatusCodes).find((code) => code.message === err.statusCode);
-
-    if (err.name === "ValidationError") {
-        const errorObj = {};
-        console.log(err);
-        for (const error of err.details) {
-            errorObj[error.path] = [error.message];
-        }
-
-        return res.status(StatusCodes.BAD_REQUEST.code).json({
-            code: 400,
-            status: StatusCodes.BAD_REQUEST.message,
-            recordsTotal: 0,
-            data: null,
-            errors: {
-                name: err.name,
-                message: err.message,
-                validation: errorObj,
-            },
-        });
-    }
-
+  if (err.name === "ValidationError") {
+    const errorObj = {};
     console.log(err);
-
-    if (err instanceof BaseError) {
-        console.error(err);
-        return res.status(statusCode.code).json({
-        code: err.errorCode,
-        status: err.statusCode,
-        recordsTotal: 0,
-        data: null,
-        errors: {
-            name: err.errorName,
-            message: err.message,
-            validation: null
-        },
-        });
+    for (const error of err.details) {
+      errorObj[error.path] = [error.message];
     }
-    console.error(err);
 
-    console.log(req.body);
-    
-    // ambil user id
-    const user = await prisma.user.findFirst({
-        where: { email: req.body.email },
-        select: { user_id: true }
-    });
-
-    // ambil ip
-    const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
-
-    // ambil device
-    const userAgent = useragent.parse(req.headers["user-agent"]);
-    const device = userAgent.toString();
-
-    const url = req.originalUrl;
-
-    // method
-    const method = req.method;
-    await prisma.systemLog.create({
-        data: {
-            userId: user ? user.user_id : null,
-            action: url,
-            ipAddress: ip,
-            device: device,
-            method: method,
-            description: err.message
-        }
-    });
-
-
-    return res.status(StatusCodes.INTERNAL_SERVER.code).json({
-        code: 500,
-        status: StatusCodes.INTERNAL_SERVER.message,
-        recordsTotal: 0,
-        data: null,
-        errors: {
+    return res.status(StatusCodes.BAD_REQUEST.code).json({
+      code: 400,
+      status: StatusCodes.BAD_REQUEST.message,
+      recordsTotal: 0,
+      data: null,
+      errors: {
         name: err.name,
         message: err.message,
-        validation: null
-        },
+        validation: errorObj,
+      },
     });
+  }
+
+  console.log(err);
+
+  if (err instanceof BaseError) {
+    console.error(err);
+    return res.status(statusCode.code).json({
+      code: err.errorCode,
+      status: err.statusCode,
+      recordsTotal: 0,
+      data: null,
+      errors: {
+        name: err.errorName,
+        message: err.message,
+        validation: null,
+      },
+    });
+  }
+  console.error(err);
+
+  console.log(req.body);
+
+  const email = req?.body?.email; // bisa undefined kalau GET
+  let user = null;
+
+  if (email) {
+    user = await prisma.user.findFirst({
+      where: { email },
+      select: { user_id: true },
+    });
+  }
+
+  // ambil ip
+  const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+
+  // ambil device
+  const userAgent = useragent.parse(req.headers["user-agent"]);
+  const device = userAgent.toString();
+
+  const url = req.originalUrl;
+
+  // method
+  const method = req.method;
+  await prisma.systemLog.create({
+    data: {
+      userId: user ? user.user_id : null,
+      action: url,
+      ipAddress: ip,
+      device: device,
+      method: method,
+      description: err.message,
+    },
+  });
+
+  return res.status(StatusCodes.INTERNAL_SERVER.code).json({
+    code: 500,
+    status: StatusCodes.INTERNAL_SERVER.message,
+    recordsTotal: 0,
+    data: null,
+    errors: {
+      name: err.name,
+      message: err.message,
+      validation: null,
+    },
+  });
 };
 
 export default errorHandler;
